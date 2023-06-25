ca63b0dd4e71c788378a72cf3b6ec7c150326b27
captain-maramo <dsfreak93@googlemail.com>
2023-06-25 15:27:48 +0200

    fixed linter problems

--- app/app/tests.py
+++ app/app/tests.py
@@ -5,6 +5,7 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTest(SimpleTestCase):
     """Test the calc module:"""
 
@@ -12,10 +13,11 @@ class CalcTest(SimpleTestCase):
         """Test adding numbers together."""
         res = calc.add(5, 6)
 
-        self.assertEqual(res,11)
+        self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
         """Test subtracting numbers"""
         res = calc.subtract(10, 15)
 
         self.assertEqual(res, 5)
+        
